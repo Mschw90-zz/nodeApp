7daa784e9364a42f122623e89d0980a7c36d129e
Matthew Schwartz <Mschw90@gmail.com>
2019-05-04 15:35:39 -0500

    env var and also cleaned up node app

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 8080
@@ -6,10 +8,66 @@ const upload = multer({ dest: __dirname+'/uploads' })
 const fs = require('fs')
 const path = require('path')
 const dirPath = path.join(__dirname, "/uploads")
+const {
+  Aborter,
+  BlobURL,
+  BlockBlobURL,
+  ContainerURL,
+  ServiceURL,
+  StorageURL,
+  SharedKeyCredential,
+  uploadStreamToBlockBlob
+} = require('@azure/storage-blob');
 
-app.post('/profile', upload.single('avatar'), function (req, res) {
-    res.redirect("/")
-})
+// const express = require('express');
+const router = express.Router();
+// const multer = require('multer');
+const inMemoryStorage = multer.memoryStorage();
+const uploadStrategy = multer({ storage: inMemoryStorage }).single('image');
+const getStream = require('into-stream');
+const containerName = 'images';
+const ONE_MEGABYTE = 1024 * 1024;
+const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
+const ONE_MINUTE = 60 * 1000;
+
+const sharedKeyCredential = new SharedKeyCredential(
+  process.env.AZURE_STORAGE_ACCOUNT_NAME,
+  process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY);
+const pipeline = StorageURL.newPipeline(sharedKeyCredential);
+const serviceURL = new ServiceURL(
+  `https://${process.env.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net`,
+  pipeline
+);
+
+const getBlobName = originalName => {
+  // Use a random number to generate a unique file name, 
+  // removing "0." from the start of the string.
+  const identifier = Math.random().toString().replace(/0\./, ''); 
+  return `${identifier}-${originalName}`;
+};
+
+app.post('/profile', uploadStrategy, async (req, res) => {
+    const aborter = Aborter.timeout(30 * ONE_MINUTE);
+    const blobName = getBlobName(req.file.originalname);
+    const stream = getStream(req.file.buffer);
+    const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
+    const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
+    
+    
+    try {
+        await uploadStreamToBlockBlob(aborter, stream,
+            blockBlobURL, uploadOptions.bufferSize, uploadOptions.maxBuffers);
+            res.redirect("/")
+            
+        } catch (err) {
+            res.json(err)
+
+  }
+});
+
+// app.post('/profile', upload.single('avatar'), function (req, res) {
+//     res.redirect("/")
+// })
 
 app.get('/images', (req, res) => {
     fs.readdir(dirPath, (err, files) => {
